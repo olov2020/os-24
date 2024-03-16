8b7fd6ec2ad31ba85554154d7f6142d10b640d7b
Vladimir <72920860+olov2020@users.noreply.github.com>
2024-03-16 17:49:30 +0300

    Update ihw.c

--- ihw1/ihw.c
+++ ihw1/ihw.c
@@ -1,45 +1,67 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 
-int is_valid_identifier(char *str) {
-    if (!isalpha(str[0])) {
-        return 0; // Проверка на начало с буквы
-    }
-    for (int i = 1; i < strlen(str); i++) {
-        if (!isalnum(str[i])) {
-            return 0; // Проверка на буквы и цифры
-        }
-    }
-    return 1;
-}
+#define BUFFER_SIZE 1024
 
 int main() {
-    char str[1000];
-    printf("Введите ASCII-строку: ");
-    fgets(str, sizeof(str), stdin);
+    char input_filename[BUFFER_SIZE];
+    char output_filename[BUFFER_SIZE];
+    int fd1[2], fd2[2];
+    pid_t pid1, pid2;
 
-    int count = 0;
-    int identifiers[1000] = {0}; // Массив для хранения уникальных идентификаторов
-    char *token = strtok(str, " ,.!?;:\n\t"); // Разделители
+    printf("Введите путь к файлу ввода: ");
+    scanf("%s", input_filename);
 
-    while (token != NULL) {
-        if (is_valid_identifier(token)) {
-            int is_new = 1;
-            for (int i = 0; i < count; i++) {
-                if (strcmp(token, &str[identifiers[i]]) == 0) {
-                    is_new = 0;
-                    break;
-                }
-            }
-            if (is_new) {
-                identifiers[count++] = token - str;
-            }
-        }
-        token = strtok(NULL, " ,.!?;:\n\t");
+    printf("Введите путь к файлу вывода: ");
+    scanf("%s", output_filename);
+
+    if (pipe(fd1) == -1 || pipe(fd2) == -1) {
+        perror("Ошибка создания канала");
+        exit(EXIT_FAILURE);
     }
 
-    printf("Количество различных идентификаторов: %d\n", count);
+    pid1 = fork();
+
+    if (pid1 < 0) {
+        perror("Ошибка при создании процесса");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid1 == 0) {
+        // Код первого процесса (читает из файла и передает через канал)
+        close(fd1[0]); // Закрываем чтение
+        dup2(fd1[1], STDOUT_FILENO); // Перенаправляем вывод в канал
+        execlp("cat", "cat", input_filename, NULL); // Читаем из файла input_filename
+        close(fd1[1]);
+    } else {
+        pid2 = fork();
+
+        if (pid2 < 0) {
+            perror("Ошибка при создании процесса");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid2 == 0) {
+            // Код второго процесса (обработка данных)
+            close(fd1[1]); // Закрываем запись
+            close(fd2[0]); // Закрываем чтение
+            dup2(fd1[0], STDIN_FILENO); // Перенаправляем ввод из канала
+            dup2(fd2[1], STDOUT_FILENO); // Перенаправляем вывод в канал
+            execlp("./process_data", "./process_data", NULL); // Запускаем программу для обработки данных
+            close(fd1[0]);
+            close(fd2[1]);
+        } else {
+            // Код третьего процесса (вывод в файл)
+            close(fd1[0]); // Закрываем чтение
+            close(fd1[1]);
+            close(fd2[1]); // Закрываем запись
+            dup2(fd2[0], STDIN_FILENO); // Перенаправляем ввод из канала
+            execlp("cat", "cat", output_filename, NULL); // Выводим в файл output_filename
+            close(fd2[0]);
+        }
+    }
 
     return 0;
 }
