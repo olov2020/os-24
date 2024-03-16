56173ea2bb6592256cd30af715533d8f59583ab0
Vladimir <72920860+olov2020@users.noreply.github.com>
2024-03-17 01:59:15 +0300

    Update solution.c

--- ihw1/solution.c
+++ ihw1/solution.c
@@ -5,21 +5,43 @@
 
 #define BUFFER_SIZE 1024
 
-int main() {
-    char input_line[BUFFER_SIZE];
-    int num_identifiers = 0;
-
-    while(fgets(input_line, sizeof(input_line), stdin) != NULL) {
-        char* token = strtok(input_line, " \t\n\r"); // Разделители: пробел, табуляция, новая строка, возврат каретки
-        while(token != NULL) {
-            if(isalpha(token[0])) { // Проверяем, начинается ли идентификатор с буквы
-                num_identifiers++;
-            }
-            token = strtok(NULL, " \t\n\r");
+int is_valid_identifier(char *str) {
+    if (!isalpha(str[0])) {
+        return 0; // Проверка на начало с буквы
+    }
+    for (int i = 1; i < strlen(str); i++) {
+        if (!isalnum(str[i])) {
+            return 0; // Проверка на буквы и цифры
         }
     }
+    return 1;
+}
 
-    printf("Количество идентификаторов: %d\n", num_identifiers);
+int main() {
+    char input_line[BUFFER_SIZE];
+    fgets(input_line, sizeof(input_line), stdin);
+
+    int count = 0;
+    int identifiers[1000] = {0}; // Массив для хранения уникальных идентификаторов
+    char *token = strtok(input_line, " ,.!?;:-=\n\t"); // Разделители
+
+    while (token != NULL) {
+        if (is_valid_identifier(token)) {
+            int is_new = 1;
+            for (int i = 0; i < count; i++) {
+                if (strcmp(token, &input_line[identifiers[i]]) == 0) {
+                    is_new = 0;
+                    break;
+                }
+            }
+            if (is_new) {
+                identifiers[count++] = token - input_line;
+            }
+        }
+        token = strtok(NULL, " ,.!?;:\n\t");
+    }
+
+    printf("Количество различных идентификаторов: %d\n", count);
 
     return 0;
 }
