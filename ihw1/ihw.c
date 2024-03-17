c4c97df928e718dd8eb94470a89c538256fe1d41
Vladimir <72920860+olov2020@users.noreply.github.com>
2024-03-17 22:55:06 +0300

    Update ihw.c

--- ihw1/ihw.c
+++ ihw1/ihw.c
@@ -3,25 +3,29 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
+#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 
 #define BUFFER_SIZE 5000
-#define FIFO_FILE "myfifo"
 
 int main() {
     char input_filename[BUFFER_SIZE];
     char output_filename[BUFFER_SIZE];
+    char fifo1[] = "/tmp/fifo1";
+    char fifo2[] = "/tmp/fifo2";
     pid_t pid1, pid2;
+
+    mkfifo(fifo1, 0666);
+    mkfifo(fifo2, 0666);
 
     printf("Введите путь к файлу ввода: ");
     fgets(input_filename, sizeof(input_filename), stdin);
-    input_filename[strcspn(input_filename, "")] = ''; // Удаляем символ новой строки
+    input_filename[strcspn(input_filename, "\n")] = '\0'; // Удаляем символ новой строки
 
     printf("Введите путь к файлу вывода: ");
     fgets(output_filename, sizeof(output_filename), stdin);
-    output_filename[strcspn(output_filename, "")] = ''; // Удаляем символ новой строки
-
-    mkfifo(FIFO_FILE, 0666);
+    output_filename[strcspn(output_filename, "\n")] = '\0'; // Удаляем символ новой строки
 
     pid1 = fork();
 
@@ -31,12 +35,13 @@ int main() {
     }
 
     if (pid1 == 0) {
-        // Код первого процесса (читает из файла и пишет в именованный канал)
-        int fd = open(FIFO_FILE, O_WRONLY);
-        execlp("cat", "cat", input_filename, NULL); // Читаем из файла input_filename и пишем в именованный канал
+        // Код первого процесса (читает из файла и передает через именованный канал)
+        int fd1 = open(fifo1, O_WRONLY);
+        dup2(fd1, STDOUT_FILENO); // Перенаправляем вывод в канал
+        execlp("cat", "cat", input_filename, NULL); // Читаем из файла input_filename
         perror("Ошибка при запуске первого процесса");
         exit(EXIT_FAILURE);
-     else {
+    } else {
         pid2 = fork();
 
         if (pid2 < 0) {
@@ -45,21 +50,30 @@ int main() {
         }
 
         if (pid2 == 0) {
-            // Код второго процесса (читает из именованного канала и обрабатывает данные)
-            int fd = open(FIFO_FILE, O_RDONLY);
-            dup2(fd, STDIN_FILENO); // Перенаправляем ввод из именованного канала
-            int output_fd = open(output_filename, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-            dup2(output_fd, STDOUT_FILENO); // Перенаправляем вывод в файл
-            execl("solution", "solution", NULL); // Запускаем программу для обработки данных
+            // Код второго процесса (обработка данных)
+            int fd1 = open(fifo1, O_RDONLY);
+            int fd2 = open(fifo2, O_WRONLY);
+            dup2(fd1, STDIN_FILENO); // Перенаправляем ввод из канала
+            dup2(fd2, STDOUT_FILENO); // Перенаправляем вывод в канал
+            execl("./solution", "solution", NULL); // Запускаем программу для обработки данных
 
             perror("Ошибка при запуске второго процесса");
             exit(EXIT_FAILURE);
-         else {
-            wait(NULL);
-            remove(FIFO_FILE); // Удаляем именованный канал
+        } else {
+            // Код третьего процесса (вывод в файл)
+            int fd2 = open(fifo2, O_RDONLY);
+            int output_fd = open(output_filename, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+            if (output_fd == -1) {
+                perror("Ошибка открытия файла вывода");
+                exit(EXIT_FAILURE);
+            }
+            dup2(fd2, STDIN_FILENO); // Перенаправляем ввод из канала
+            dup2(output_fd, STDOUT_FILENO); // Перенаправляем вывод в файл
+            execlp("cat", "cat", NULL); // Выводим в файл output_filename
+            perror("Ошибка при запуске третьего процесса");
+            exit(EXIT_FAILURE);
         }
     }
-     }
-    }
+
     return 0;
 }
