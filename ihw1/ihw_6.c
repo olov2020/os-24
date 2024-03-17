f69f01d632cc13f01301d716c78fe3f76a95d403
Vladimir <72920860+olov2020@users.noreply.github.com>
2024-03-17 23:30:22 +0300

    Update ihw_6.c

--- ihw1/ihw_6.c
+++ ihw1/ihw_6.c
@@ -12,12 +12,11 @@
 int main() {
     char input_filename[BUFFER_SIZE];
     char output_filename[BUFFER_SIZE];
-    int channel1[2];
-    int channel2[2];
+    int pipefd[2];
     pid_t pid1, pid2;
 
-    if (pipe(channel1) == -1 || pipe(channel2) == -1) {
-        perror("Ошибка при создании каналов");
+    if (pipe(pipefd) == -1) {
+        perror("Ошибка при создании канала");
         exit(EXIT_FAILURE);
     }
 
@@ -38,15 +37,15 @@ int main() {
 
     if (pid1 == 0) {
         // Код первого процесса (читает из файла и передает через неименованный канал)
-        close(channel1[0]);  // Закрываем чтение канала
+        close(pipefd[0]);  // Закрываем чтение
         int input_fd = open(input_filename, O_RDONLY);
         char buffer[BUFFER_SIZE];
         ssize_t bytes_read;
         while ((bytes_read = read(input_fd, buffer, BUFFER_SIZE)) > 0) {
-            write(channel1[1], buffer, bytes_read);
+            write(pipefd[1], buffer, bytes_read);
         }
         close(input_fd);
-        close(channel1[1]);
+        close(pipefd[1]);
         exit(EXIT_SUCCESS);
     } else {
         pid2 = fork();
@@ -58,25 +57,26 @@ int main() {
 
         if (pid2 == 0) {
             // Код второго процесса (обработка данных)
-            close(channel1[1]);  // Закрываем запись канала 1
-            close(channel2[0]);  // Закрываем чтение канала 2
-            dup2(channel1[0], STDIN_FILENO);
-            dup2(channel2[1], STDOUT_FILENO);
-            execl("./solution", "solution", NULL);
-            perror("Ошибка при запуске второго процесса");
-            exit(EXIT_FAILURE);
+            char buffer[BUFFER_SIZE];
+            ssize_t bytes_read;
+            close(pipefd[1]);  // Закрываем запись
+            while ((bytes_read = read(pipefd[0], buffer, BUFFER_SIZE)) > 0) {
+                // Обработка данных в соответствии с заданием
+                // Здесь можно вставить вашу обработку
+                // В данном примере я просто отправлю данные обратно
+                write(STDOUT_FILENO, buffer, bytes_read);
+            }
+            close(pipefd[0]);
+            exit(EXIT_SUCCESS);
         } else {
             // Код третьего процесса (вывод в файл)
-            close(channel1[0]);
-            close(channel2[1]);
             int output_fd = open(output_filename, O_WRONLY | O_CREAT | O_TRUNC, 0666);
             char buffer[BUFFER_SIZE];
             ssize_t bytes_read;
-            while ((bytes_read = read(channel2[0], buffer, BUFFER_SIZE)) > 0) {
+            while ((bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE)) > 0) {
                 write(output_fd, buffer, bytes_read);
             }
             close(output_fd);
-            close(channel2[0]);
             exit(EXIT_SUCCESS);
         }
     }
