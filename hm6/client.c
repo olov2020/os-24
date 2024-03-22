9c5d6423b8fb41cb6253633db572ddb55ed9ad6e
Vladimir <72920860+olov2020@users.noreply.github.com>
2024-03-22 21:31:00 +0300

    Create client.c

---
+++ hm6/client.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define SHMSZ 27
+
+int main()
+{
+    key_t key;
+    int shmid;
+    char *shm, *s;
+
+    key = ftok(".", 'B');
+
+    if ((shmid = shmget(key, SHMSZ, IPC_CREAT | 0666)) < 0)
+    {
+        perror("shmget");
+        exit(1);
+    }
+
+    if ((shm = shmat(shmid, NULL, 0)) == (char *)-1)
+    {
+        perror("shmat");
+        exit(1);
+    }
+
+    s = shm;
+
+    srand(getpid());
+
+    int random_num = rand() % 1000 + 1;
+
+    *s = random_num;
+
+    while (*shm != '*')
+    {
+        sleep(1);
+    }
+
+    exit(0);
+}
