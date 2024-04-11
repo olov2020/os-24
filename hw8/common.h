3fda612607991f2f9e753a84db8ca9d5f95d74d0
Vladimir <72920860+olov2020@users.noreply.github.com>
2024-04-12 01:10:43 +0300

    Update common.h

--- hw8/common.h
+++ hw8/common.h
@@ -1,4 +1,3 @@
-// Заголовочный файл, содержащий общие данные для писателей и читателей
 #include <time.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -9,53 +8,37 @@
 #include <semaphore.h>
 #include <signal.h>
 
-#define BUF_SIZE 10     // Размер буфера ячеек
-// Структура для хранения в разделяемой памяти буфера и необходимых данных
-// Задает буфер, доступный процессам
+#define BUF_SIZE 10 
 typedef struct {
   int store[BUF_SIZE];  // буфер для заполнения ячеек
-  int next_write;       // индекс для записи в буфер
-  int next_read;        // индекс для чтения из буфера
-  int have_reader;      // индикатор наличия читателя
-  int reader_pid;       // идентификатор процесса читателя
-  int writer_pid;       // идентификатор процесса писателя
+  int next_write;   // индекс для записи в буфер
+  int next_read;   // индекс для чтения из буфера
+  int have_reader;  // индикатор наличия читателя
+  int reader_pid;  // идентификатор процесса читателя
+  int writer_pid;  // идентификатор процесса писателя
 } shared_memory;
 
-// имя области разделяемой памяти
 extern const char* shar_object ;
-extern int buf_id;        // дескриптор объекта памяти
-extern shared_memory *buffer;    // указатель на разделямую память, хранящую буфер
+extern int buf_id;       
+extern shared_memory *buffer;  
 
-// имя семафора для занятых ячеек
 extern const char *full_sem_name;
-extern sem_t *full;   // указатель на семафор занятых ячеек
+extern sem_t *full;  
 
-// имя семафора для свободных ячеек
 extern const char *empty_sem_name;
-extern sem_t *empty;   // указатель на семафор свободных ячеек
+extern sem_t *empty;
 
-// имя семафора (мьютекса) для критической секции,
-// обеспечивающей доступ писателя к буферу
 extern const char *mutex_write_sem_name;
-extern sem_t *write_mutex;   // указатель на семафор писателей
+extern sem_t *write_mutex;
 
-// имя семафора (мьютекса) для критической секции,
-// обеспечивающей доступ писателя к буферу
 extern const char *mutex_read_sem_name;
-extern sem_t *read_mutex;   // указатель на семафор читателей
+extern sem_t *read_mutex; 
 
-// Имя семафора для управления доступом.
-// Позволяет читателю дождаться разрешения от читателя.
-// Даже если читатель стартовал первым
 extern const char *admin_sem_name;
-extern sem_t *admin;   // указатель на семафор читателей
+extern sem_t *admin; 
 
-// Функция осуществляющая при запуске общие манипуляции с памятью и семафорами
-// для децентрализованно подключаемых процессов читателей и писателей.
 void init(void);
 
-// Функция закрывающая семафоры общие для писателей и читателей
 void close_common_semaphores(void);
 
-// Функция, удаляющая все семафоры и разделяемую память
 void unlink_all(void);
