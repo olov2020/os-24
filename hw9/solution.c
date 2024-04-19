de9e97c1693ea55ad2ca8d83e38fe7284191dfde
Vladimir <72920860+olov2020@users.noreply.github.com>
2024-04-19 20:36:25 +0300

    Update solution.c

--- hw9/solution.c
+++ hw9/solution.c
@@ -1,57 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <semaphore.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
 
-#define MESSAGE_COUNT 10
+#define NUM_MESSAGES 10
+
+int sem_id;
+
+void parent_process(int pipe_fd[2]) {
+    int i;
+    char message[256];
+    
+    for (i = 0; i < NUM_MESSAGES; ++i) {
+        printf("Parent process sending message %d\n", i);
+        sprintf(message, "Message from parent %d", i);
+        write(pipe_fd[1], message, sizeof(message));
+        sleep(1);
+        read(pipe_fd[0], message, sizeof(message));
+        printf("Parent process received: %s\n", message);
+    }
+
+    // Отправка сигнала завершения дочернему процессу
+    kill(getpid(), SIGINT);
+}
+
+void child_process(int pipe_fd[2]) {
+    int i;
+    char message[256];
+    
+    for (i = 0; i < NUM_MESSAGES; ++i) {
+        read(pipe_fd[0], message, sizeof(message));
+        printf("Child process received: %s\n", message);
+        sleep(1);
+        printf("Child process sending message %d\n", i);
+        sprintf(message, "Message from child %d", i);
+        write(pipe_fd[1], message, sizeof(message));
+    }
+}
+
+void signal_handler(int sig) {
+    printf("Signal received, exiting.\n");
+    // Удаление семафора
+    semctl(sem_id, 0, IPC_RMID);
+    exit(0);
+}
 
 int main() {
-    int pipefd[2];
-    if (pipe(pipefd) == -1) {
-        perror("pipe");
-        exit(EXIT_FAILURE);
+    int pipe_fd[2];
+
+    // Создание канала
+    if (pipe(pipe_fd) == -1) {
+        perror("Pipe creation failed");
+        return 1;
     }
 
-    sem_t semaphore;
-    if (sem_init(&semaphore, 1, 0) == -1) {
-        perror("sem_init");
-        exit(EXIT_FAILURE);
-    }
+    // Установка обработчика сигнала
+    signal(SIGINT, signal_handler);
 
+    // Создание дочернего процесса
     pid_t pid = fork();
+
     if (pid == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
+        perror("Fork failed");
+        return 1;
+    } else if (pid > 0) {
+        // Родительский процесс
+        close(pipe_fd[0]);  // Закрыть чтение в родительском процессе
+        parent_process(pipe_fd);
+    } else {
+        // Дочерний процесс
+        close(pipe_fd[1]);  // Закрыть запись в дочернем процессе
+        child_process(pipe_fd);
     }
 
-    if (pid == 0) { // Child process
-        close(pipefd[1]); // Close write end
-        int i;
-        for (i = 0; i < MESSAGE_COUNT; i++) {
-            sem_wait(&semaphore); // Wait for signal from parent
-            char message[100];
-            read(pipefd[0], message, sizeof(message));
-            printf("Child received: %s\n", message);
-            sem_post(&semaphore); // Signal parent
-        }
-        close(pipefd[0]); // Close read end
-        sem_destroy(&semaphore);
-        exit(EXIT_SUCCESS);
-    } else { // Parent process
-        close(pipefd[0]); // Close read end
-        int i;
-        for (i = 0; i < MESSAGE_COUNT; i++) {
-            char message[100];
-            sprintf(message, "Message %d from parent", i+1);
-            write(pipefd[1], message, sizeof(message));
-            sem_post(&semaphore); // Signal child
-            sem_wait(&semaphore); // Wait for signal from child
-        }
-        close(pipefd[1]); // Close write end
-        sem_destroy(&semaphore);
-        wait(NULL); // Wait for child to finish
-        exit(EXIT_SUCCESS);
-    }
+    return 0;
 }
