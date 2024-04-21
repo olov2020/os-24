f67804f50dbfc3dd9161529852ed7fbd7d3541ba
Vladimir <72920860+olov2020@users.noreply.github.com>
2024-04-21 22:59:06 +0300

    Update solution.c

--- ihw2/solution.c
+++ ihw2/solution.c
@@ -3,10 +3,13 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
-#define M 10 // Количество кусков тушеного миссионера
-#define N 5 // Количество дикарей
-
 int main() {
+    int M, N;
+    printf("Введите количество кусков тушеного миссионера (M): ");
+    scanf("%d", &M);
+    printf("Введите количество дикарей (N): ");
+    scanf("%d", &N);
+
     int pot = M; // Горшок с кусками тушеного миссионера
     int pid;
 
@@ -16,7 +19,6 @@ int main() {
             perror("fork failed");
             exit(1);
         } else if (pid == 0) {
-            // Дикарь
             while (1) {
                 if (pot == 0) {
                     printf("Дикарь %d будит повара\n", i + 1);
@@ -25,19 +27,20 @@ int main() {
                     printf("Дикарь %d ест кусок тушеного миссионера. Осталось: %d\n", i + 1, pot);
                     pot--;
                 }
+                sleep(1);
             }
         }
     }
 
     // Повар
     while (1) {
-        sleep(1); // Повар засыпает
+        sleep(1);
         printf("Повар заполняет горшок\n");
         pot = M;
     }
 
     for (int i = 0; i < N; i++) {
-        wait(NULL); // Ждем завершения всех дикарей
+        wait(NULL);
     }
 
     return 0;
