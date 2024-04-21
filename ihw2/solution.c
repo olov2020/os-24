e1c84f32356250f31ba9343015aa04a4df4911cf
Vladimir <72920860+olov2020@users.noreply.github.com>
2024-04-21 22:43:58 +0300

    Update solution.c

--- ihw2/solution.c
+++ ihw2/solution.c
@@ -2,97 +2,44 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <semaphore.h>
 
-sem_t pot_sem; // Semaphore for access to the pot
-sem_t cook_sem; // Semaphore for controlling the cook
-int remaining_pieces = 0; // Number of remaining pieces of stewed missionary
+#define N 5 // Количество дикарей
+#define M 10 // Вместимость горшка
 
-void cook(int M) {
-    printf("Cook starts preparing lunch.\n");
-    remaining_pieces = M;
-    printf("Cook prepared %d pieces of stewed missionary.\n", remaining_pieces);
-    sem_post(&pot_sem); // Release the semaphore so the tribesmen can start eating
+int pot = 0;
+
+void cook() {
+    printf("Повар начал готовить обед.\n");
+    usleep(3000000); // Имитация приготовления еды
+    pot = M;
+    printf("Обед готов!\n");
 }
 
 void eat(int id) {
-    printf("Tribesman %d is hungry and wants to have lunch.\n", id);
-    sem_wait(&pot_sem); // Wait for access to the pot
-    if (remaining_pieces > 0) {
-        printf("Tribesman %d took a piece of stewed missionary from the pot.\n", id);
-        remaining_pieces--;
-    } else {
-        printf("The pot is empty, tribesman %d wakes up the cook.\n", id);
-        sem_post(&cook_sem); // Tribesman wakes up the cook
-        sem_wait(&pot_sem); // Wait until the cook fills the pot
-        printf("Tribesman %d took a piece of stewed missionary from the pot.\n", id);
-        remaining_pieces--;
+    printf("Дикарь %d начал обедать.\n", id);
+    while (pot == 0) {
+        printf("Дикарь %d будил повара.\n", id);
+        cook();
     }
-    printf("Tribesman %d had lunch.\n", id);
-}
-
-void cook_process(int M) {
-    while (1) {
-        sem_wait(&cook_sem); // Wait until a tribesman wakes up the cook
-        cook(M);
-    }
-}
-
-void eat_process(int id, int N) {
-    while (1) {
-        eat(id);
-        usleep(rand() % 1000000); // Tribesman will be hungry for a random time before the next meal
-    }
+    pot--;
+    printf("Дикарь %d закончил обедать.\n", id);
 }
 
 int main() {
-    int N, M;
-
-    printf("Enter the number of tribesmen (N): ");
-    if (scanf("%d", &N) != 1 || N <= 0) {
-        printf("Invalid input for the number of tribesmen.\n");
-        return 1;
-    }
-
-    printf("Enter the number of pieces of stewed missionary (M): ");
-    if (scanf("%d", &M) != 1 || M <= 0) {
-        printf("Invalid input for the number of pieces.\n");
-        return 1;
-    }
-
-    sem_init(&pot_sem, 0, 1); // Semaphore initialization
-    sem_init(&cook_sem, 0, 0); // Initialize cook semaphore
-
     pid_t pid;
-    for (int i = 0; i < N; i++) {
+
+    for (int i = 1; i <= N; i++) {
         pid = fork();
-        if (pid == -1) {
-            perror("fork");
+        if (pid < 0) {
+            perror("fork failed");
             exit(1);
-        }
-        if (pid == 0) { // Child process
-            eat_process(i+1, N);
+        } else if (pid == 0) {
+            eat(i);
             exit(0);
         }
     }
 
-    pid = fork();
-    if (pid == -1) {
-        perror("fork");
-        exit(1);
-    }
-    if (pid == 0) { // Child process
-        cook_process(M);
-        exit(0);
-    }
-
-    // Parent process waits for all child processes to finish
-    for (int i = 0; i < N + 1; i++) {
-        wait(NULL);
-    }
-
-    sem_destroy(&pot_sem); // Semaphore destruction
-    sem_destroy(&cook_sem);
+    while (wait(NULL) > 0); // Ждем завершения всех процессов дикарей
 
     return 0;
 }
