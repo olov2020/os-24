ff80f70d31745572ecc578fad3b13c2b20da94fa
Vladimir <72920860+olov2020@users.noreply.github.com>
2024-05-11 13:21:43 +0300

    Update server.c

--- hw10/server.c
+++ hw10/server.c
@@ -3,89 +3,78 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
-int main(int argc, char *argv[]) {
+#define PORT 8080
+
+int main(int argc, char** argv) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <server_ip_address> <server_port>\n", argv[0]);
-        return EXIT_FAILURE;
+        printf("Usage: %s <server IP> <server port>n", argv[0]);
+        return 1;
     }
+
+    int server_sock, client_sock;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_addr_len;
+    char buffer[1024];
 
     // Создание сокета сервера
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
+    server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock < 0) {
         perror("socket");
-        return EXIT_FAILURE;
+        return 1;
     }
 
-    // Настройка адреса сервера
-    struct sockaddr_in server_address;
-    memset(&server_address, 0, sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = inet_addr(argv[1]);
-    server_address.sin_port = htons(atoi(argv[2]));
+    // Настройка адреса и порта сервера
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    server_addr.sin_port = htons(atoi(argv[2]));
 
-    // Привязка сокета к адресу
-    if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
+    // Привязка сокета к адресу и порту
+    if (bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         perror("bind");
-        return EXIT_FAILURE;
+        return 1;
     }
 
-    // Слушаем входящие подключения
-    if (listen(server_socket, 5) == -1) {
+    // Прослушивание сокета
+    if (listen(server_sock, 5) < 0) {
         perror("listen");
-        return EXIT_FAILURE;
+        return 1;
     }
 
+    // Бесконечный цикл для обработки клиентов
     while (1) {
-        // Принимаем соединение от клиента №1
-        int client1_socket = accept(server_socket, NULL, NULL);
-        if (client1_socket == -1) {
+        // Принятие соединения от клиента
+        client_addr_len = sizeof(client_addr);
+        client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_addr_len);
+        if (client_sock < 0) {
             perror("accept");
             continue;
         }
 
-        // Принимаем соединение от клиента №2
-        int client2_socket = accept(server_socket, NULL, NULL);
-        if (client2_socket == -1) {
-            perror("accept");
-            close(client1_socket);
+        // Чтение сообщения от клиента
+        ssize_t bytes_received = recv(client_sock, buffer, sizeof(buffer), 0);
+        if (bytes_received < 0) {
+            perror("recv");
+            close(client_sock);
             continue;
         }
 
-        // Создаем поток для перенаправления сообщений
-        int pid = fork();
-        if (pid == 0) {
-            // Поток для перенаправления сообщений
-            char buffer[1024];
-
-            // Перенаправляем сообщения от клиента №1 клиенту №2
-            while (1) {
-                memset(buffer, 0, sizeof(buffer));
-                if (recv(client1_socket, buffer, sizeof(buffer), 0) <= 0) {
-                    break;
-                }
-                send(client2_socket, buffer, strlen(buffer), 0);
-            }
-
-            // Закрываем сокеты
-            close(client1_socket);
-            close(client2_socket);
-
-            exit(0);
-        } else if (pid > 0) {
-            // Родительский процесс, ожидающий завершения потока
-            int status;
-            waitpid(pid, &status, 0);
-        } else {
-            // Ошибка создания потока
-            perror("fork");
-            close(client1_socket);
-            close(client2_socket);
+        // Проверка сообщения на завершение работы
+        if (strcmp(buffer, "The End") == 0) {
+            printf("Received "The End" from client %s:%dn", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+            break;
         }
+
+        // Пересылка сообщения клиенту №2
+        // ... (код для подключения к клиенту №2 и отправки сообщения)
+
+        // Закрытие сокета клиента
+        close(client_sock);
     }
 
-    // Закрываем сокет сервера
-    close(server_socket);
+    // Закрытие сокета сервера
+    close(server_sock);
 
-    return EXIT_SUCCESS;
+    return 0;
 }
