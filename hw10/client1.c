d0cf57f48e9bc86125c7bbc535b949e5251055f4
Vladimir <72920860+olov2020@users.noreply.github.com>
2024-05-11 14:06:17 +0300

    Update client1.c

--- hw10/client1.c
+++ hw10/client1.c
@@ -12,45 +12,35 @@ int main() {
     char *end_message = "The End";
     char message[1024];
 
-    // Создание сокета клиента
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Socket creation error \n");
         return -1;
     }
 
-    // Настройка адреса сервера
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    // Преобразование IP-адреса в двоичный формат
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
 
-    // Установление соединения с сервером
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
         printf("\nConnection Failed \n");
         return -1;
     }
 
-    // Цикл отправки сообщений
     while (1) {
-        // Ввод сообщения
         printf("Enter message for Client 2: ");
         fgets(message, 1024, stdin);
+        send(sock, message, strlen(message), 0);
 
-        // Проверка завершения работы
         if (strcmp(message, end_message) == 0) {
             printf("End of communication\n");
             break;
         }
-
-        // Отправка сообщения серверу
-        send(sock, message, strlen(message), 0);
     }
 
-    // Закрытие сокета
     close(sock);
     return 0;
 }
