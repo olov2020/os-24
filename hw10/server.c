8b99e01c1976bffe292e4059178716ccfc7246f4
Vladimir <72920860+olov2020@users.noreply.github.com>
2024-05-11 13:13:05 +0300

    Update server.c

--- hw10/server.c
+++ hw10/server.c
@@ -1,77 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <string.h>
 
-#define PORT 8080
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <server_ip_address> <server_port>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
 
-int main(int argc, char** argv) {
-    int server_sockfd, client1_sockfd, client2_sockfd;
-    struct sockaddr_in server_addr, client1_addr, client2_addr;
-    socklen_t client1_addr_len, client2_addr_len;
-    char buffer[1024];
-
-    // Создать серверный сокет
-    server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_sockfd < 0) {
+    // Создание сокета сервера
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
         perror("socket");
-        exit(1);
+        return EXIT_FAILURE;
     }
 
-    // Настроить серверный сокет
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
+    // Настройка адреса сервера
+    struct sockaddr_in server_address;
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = inet_addr(argv[1]);
+    server_address.sin_port = htons(atoi(argv[2]));
 
-    // Привязать сокет к адресу
-    if (bind(server_sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+    // Привязка сокета к адресу
+    if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
         perror("bind");
-        exit(1);
+        return EXIT_FAILURE;
     }
 
-    // Начать прослушивание
-    if (listen(server_sockfd, 5) < 0) {
+    // Слушаем входящие подключения
+    if (listen(server_socket, 5) == -1) {
         perror("listen");
-        exit(1);
+        return EXIT_FAILURE;
     }
 
-    // Принять подключение от клиента №1
-    client1_addr_len = sizeof(client1_addr);
-    client1_sockfd = accept(server_sockfd, (struct sockaddr*)&client1_addr, &client1_addr_len);
-    if (client1_sockfd < 0) {
-        perror("accept");
-        exit(1);
-    }
-
-    // Принять подключение от клиента №2
-    client2_addr_len = sizeof(client2_addr);
-    client2_sockfd = accept(server_sockfd, (struct sockaddr*)&client2_addr, &client2_addr_len);
-    if (client2_sockfd < 0) {
-        perror("accept");
-        exit(1);
-    }
-
-    // Обрабатывать сообщения до получения сообщения "The End"
     while (1) {
-        // Получить сообщение от клиента №1
-        memset(buffer, 0, sizeof(buffer));
-        recv(client1_sockfd, buffer, sizeof(buffer), 0);
-
-        // Проверить, является ли сообщение сообщением завершения
-        if (strcmp(buffer, "The End") == 0) {
-            break;
+        // Принимаем соединение от клиента №1
+        int client1_socket = accept(server_socket, NULL, NULL);
+        if (client1_socket == -1) {
+            perror("accept");
+            continue;
         }
 
-        // Перенаправить сообщение клиенту №2
-        send(client2_sockfd, buffer, strlen(buffer), 0);
+        // Принимаем соединение от клиента №2
+        int client2_socket = accept(server_socket, NULL, NULL);
+        if (client2_socket == -1) {
+            perror("accept");
+            close(client1_socket);
+            continue;
+        }
+
+        // Создаем поток для перенаправления сообщений
+        int pid = fork();
+        if (pid == 0) {
+            // Поток для перенаправления сообщений
+            char buffer[1024];
+
+            // Перенаправляем сообщения от клиента №1 клиенту №2
+            while (1) {
+                memset(buffer, 0, sizeof(buffer));
+                if (recv(client1_socket, buffer, sizeof(buffer), 0) <= 0) {
+                    break;
+                }
+                send(client2_socket, buffer, strlen(buffer), 0);
+            }
+
+            // Закрываем сокеты
+            close(client1_socket);
+            close(client2_socket);
+
+            exit(0);
+        } else if (pid > 0) {
+            // Родительский процесс, ожидающий завершения потока
+            int status;
+            waitpid(pid, &status, 0);
+        } else {
+            // Ошибка создания потока
+            perror("fork");
+            close(client1_socket);
+            close(client2_socket);
+        }
     }
 
-    // Закрыть сокеты
-    close(client1_sockfd);
-    close(client2_sockfd);
-    close(server_sockfd);
+    // Закрываем сокет сервера
+    close(server_socket);
 
-    return 0;
+    return EXIT_SUCCESS;
 }
