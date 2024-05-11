26b7e32641e39c367fc6a3362a8a35ef3212b55a
Vladimir <72920860+olov2020@users.noreply.github.com>
2024-05-11 14:06:38 +0300

    Update server.c

--- hw10/server.c
+++ hw10/server.c
@@ -14,57 +14,45 @@ int main() {
     char buffer[1024] = {0};
     char *end_message = "The End";
 
-    // Создание сокета сервера
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Настройка параметров сокета
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
 
-    // Настройка адреса сервера
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // Привязка сокета к адресу
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    // Установка сокета в режим прослушивания
     if (listen(server_fd, 3) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
-    // Прием соединения от клиента №1
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
         perror("accept");
         exit(EXIT_FAILURE);
     }
 
-    // Цикл обработки сообщений
     while (1) {
-        // Получение сообщения от клиента №1
         read(new_socket, buffer, 1024);
+        printf("Client 1: %s\n", buffer);
 
-        // Проверка завершения работы
         if (strcmp(buffer, end_message) == 0) {
             printf("End of communication\n");
             break;
         }
-
-        // Отправка сообщения клиенту №2
-        send(new_socket, buffer, strlen(buffer), 0);
     }
 
-    // Закрытие сокетов
     close(new_socket);
     close(server_fd);
     return 0;
