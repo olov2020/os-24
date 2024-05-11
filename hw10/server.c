d7d512ccf6e2f5a3640113d13edebed1a7fd5879
Vladimir <72920860+olov2020@users.noreply.github.com>
2024-05-11 13:36:53 +0300

    Update server.c

--- hw10/server.c
+++ hw10/server.c
@@ -14,45 +14,54 @@ int main() {
     char buffer[1024] = {0};
     char *end_message = "The End";
 
+    // Create a socket.
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
+    // Allow the socket to be reused (prevents "address already in use" errors).
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
 
+    // Initialize the address structure.
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
+    // Bind the socket to the address.
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
+    // Listen for incoming connections.
     if (listen(server_fd, 3) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
+    // Accept an incoming connection.
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
         perror("accept");
         exit(EXIT_FAILURE);
     }
 
+    // Receive messages from the client and print them to the console.
     while (1) {
         read(new_socket, buffer, 1024);
         printf("Client 1: %s\n", buffer);
 
+        // Check if the client has sent the "The End" message.
         if (strcmp(buffer, end_message) == 0) {
             printf("End of communication\n");
             break;
         }
     }
 
+    // Close the new socket and the server socket.
     close(new_socket);
     close(server_fd);
     return 0;
