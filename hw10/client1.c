b4b0356ebdf3b275a652a8cb4861196a1ea16107
Vladimir <72920860+olov2020@users.noreply.github.com>
2024-05-11 13:30:37 +0300

    Update client1.c

--- hw10/client1.c
+++ hw10/client1.c
@@ -1,55 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <server_ip> <server_port>\n", argv[0]);
-        exit(1);
+#define PORT 8080
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char *end_message = "The End";
+    char message[1024];
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
     }
 
-    // Создать сокет клиента
-    int client_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_sock == -1) {
-        perror("socket");
-        exit(1);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
     }
 
-    // Настроить адрес и порт сервера
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(argv[1]);
-    server_addr.sin_port = htons(atoi(argv[2]));
-
-    // Установить соединение с сервером
-    if (connect(client_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        perror("connect");
-        exit(1);
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
     }
 
-    // В цикле читать сообщения с консоли и отправлять их серверу
-    char buffer[1024];
     while (1) {
-        // Прочитать сообщение с консоли
-        printf("Введите сообщение: ");
-        fgets(buffer, sizeof(buffer), stdin);
+        printf("Enter message for Client 2: ");
+        fgets(message, 1024, stdin);
+        send(sock, message, strlen(message), 0);
 
-        // Отправить сообщение серверу
-        if (send(client_sock, buffer, strlen(buffer), 0) == -1) {
-            perror("send");
-            exit(1);
-        }
-
-        if (strcmp(buffer, "The End\n") == 0) {
-            // Отправили сообщение "The End" - завершаем работу
-            close(client_sock);
-            exit(0);
+        if (strcmp(message, end_message) == 0) {
+            printf("End of communication\n");
+            break;
         }
     }
 
+    close(sock);
     return 0;
 }
