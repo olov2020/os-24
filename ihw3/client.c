7c34598ff42ebbf1b5eab9b68f50410df06f8015
Vladimir <72920860+olov2020@users.noreply.github.com>
2024-05-22 19:06:15 +0300

    Update client.c

--- ihw3/client.c
+++ ihw3/client.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <server_ip>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    // Создать сокет
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Подключиться к серверу
     struct sockaddr_in serv_addr;
-    
-    int client_fd;
-
-    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-    
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(8080);
-    
-    if (connect(client_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("Connection failed");
+    serv_addr.sin_port = htons(5000);
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("connect");
         exit(EXIT_FAILURE);
     }
-    
-    printf("Дикарь присоединился к обеду\n");
 
-    // Логика клиента (дикаря)
-    char buffer[1024] = {0};
-    int valread;
+    while (1) {
+        // Ждать сообщения от повара о том, что горшок полон
+        char msg[256];
+        recv(sockfd, msg, sizeof(msg), 0);
 
-    // Отправляем запрос на сервер
-    send(client_fd, "Запрос: Хочу съесть кусок миссионера", 35, 0);
-    
-    // Получаем ответ от сервера
-    valread = read(client_fd, buffer, 1024);
-    printf("Ответ от повара: %s\n", buffer);
+        // Если горшок полон, есть один кусок мяса
+        if (strcmp(msg, "Горшок полон") == 0) {
+            printf("Дикарь поел\n");
+        }
+    }
 
-    // Закрытие клиентского сокета
-    close(client_fd);
+    close(sockfd);
 
     return 0;
 }
