742fe083d5d44cfb43477f5833ff26d7940a350c
Vladimir <72920860+olov2020@users.noreply.github.com>
2024-05-22 20:19:27 +0300

    Update and rename solution.c to solution.cpp

---
+++ ihw3/solution.cpp
@@ -0,0 +1,69 @@
+#include <QApplication>
+#include <QWidget>
+#include <QPushButton>
+#include <QTcpSocket>
+#include <QTextEdit>
+
+class ClientApp : public QWidget
+{
+    Q_OBJECT
+
+public:
+    ClientApp(QWidget *parent = nullptr) : QWidget(parent)
+    {
+        tcpSocket = new QTcpSocket(this);
+
+        connect(tcpSocket, &QTcpSocket::readyRead, this, &ClientApp::readData);
+
+        connectButton = new QPushButton("Connect", this);
+        connect(connectButton, &QPushButton::clicked, this, &ClientApp::connectToServer);
+
+        sendButton = new QPushButton("Send Request", this);
+        connect(sendButton, &QPushButton::clicked, this, &ClientApp::sendRequest);
+
+        textEdit = new QTextEdit(this);
+
+        QVBoxLayout *layout = new QVBoxLayout(this);
+        layout->addWidget(connectButton);
+        layout->addWidget(sendButton);
+        layout->addWidget(textEdit);
+
+        setLayout(layout);
+    }
+
+private slots:
+    void connectToServer()
+    {
+        tcpSocket->connectToHost("127.0.0.1", 8080);
+    }
+
+    void sendRequest()
+    {
+        QString request = "Дикарь: Я хочу съесть миссионера";
+        tcpSocket->write(request.toUtf8());
+    }
+
+    void readData()
+    {
+        QByteArray data = tcpSocket->readAll();
+        textEdit->append(data);
+    }
+
+private:
+    QTcpSocket *tcpSocket;
+    QPushButton *connectButton;
+    QPushButton *sendButton;
+    QTextEdit *textEdit;
+};
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    ClientApp clientApp;
+    clientApp.show();
+
+    return app.exec();
+}
+
+#include "main.moc"
