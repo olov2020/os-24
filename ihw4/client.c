7ab1e917130069554f50f0fc675825885a634c7d
Vladimir <72920860+olov2020@users.noreply.github.com>
2024-06-06 19:35:06 +0300

    Create client.c

---
+++ ihw4/client.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+int main() {
+    struct sockaddr_in serv_addr;
+    int client_fd;
+    char *request = "Дикарь: Я хочу съесть миссионера";
+
+    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(8080);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(client_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    send(client_fd, request, strlen(request), 0);
+
+    char buffer[1024] = {0};
+    read(client_fd, buffer, 1024);
+    printf("%s\n", buffer);
+
+    close(client_fd);
+
+    return 0;
+}
